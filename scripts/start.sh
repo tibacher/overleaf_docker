4e370851eef398bfb99ffb56d27da6d8f3fe4254
Tim Schäfer <tim.schaefer.de@gmail.com>
2023-01-05 13:12:36 +0100

    fixed for new docker version

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ RC=0
 docker_path=`realpath $(dirname $(readlink -f $0))/../` 
 cd $docker_path
 
-/usr/bin/docker-compose up -d --remove-orphans || RC=1
+docker compose up -d --remove-orphans || RC=1
 
 sleep 120 && sudo monit monitor sharelatex & sudo monit monitor sharelatex-local &
 exit $RC
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,6 +5,6 @@ docker_path=`realpath $(dirname $(readlink -f $0))/../`
 cd $docker_path
 
 sudo monit unmonitor sharelatex && sudo monit unmonitor sharelatex-local
-/usr/bin/docker-compose down -v --remove-orphans || RC=1
+docker compose down -v --remove-orphans || RC=1
 #rm proxy/conf.d/default.conf
 exit $RC
